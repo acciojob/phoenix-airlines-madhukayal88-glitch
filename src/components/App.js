13ce3ec87a11825523a807856a9845d794893dc5
kayalvizhi <madhukayal88@gmail.com>
2026-08-12 05:15:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import FlightSearch from './components/FlightSearch';
+import FlightBooking from './components/FlightBooking';
+import Confirmation from './components/Confirmation';
+import './styles.css';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app">
+      <nav className="navbar">
+        <div className="nav-brand">✈️ Phoenix Airlines</div>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/flight-search">Search Flights</Link>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/flight-search" element={<FlightSearch />} />
+        <Route path="/flight-booking" element={<FlightBooking />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
