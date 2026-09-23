619e524f3c080b6f7e3268577d6c3f350370398a
kayalvizhi <madhukayal88@gmail.com>
2026-09-23 07:20:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import FlightSearch from './components/FlightSearch';
-import FlightBooking from './components/FlightBooking';
-import Confirmation from './components/Confirmation';
-import './styles.css';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import FlightSearch from './pages/FlightSearch';
+import FlightBooking from './pages/FlightBooking';
+import Confirmation from './pages/Confirmation';
 
 function App() {
   return (
-    <div className="app">
-      <nav className="navbar">
-        <div className="nav-brand">✈️ Phoenix Airlines</div>
-        <div className="nav-links">
-          <Link to="/">Home</Link>
-          <Link to="/flight-search">Search Flights</Link>
-        </div>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/flight-search" element={<FlightSearch />} />
-        <Route path="/flight-booking" element={<FlightBooking />} />
-        <Route path="/confirmation" element={<Confirmation />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/flight-search" element={<FlightSearch />} />
+      <Route path="/flight-booking" element={<FlightBooking />} />
+      <Route path="/confirmation" element={<Confirmation />} />
+    </Routes>
   );
 }
 
